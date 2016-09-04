428f0642a383e3fbf449bf77f869e2a4433cef70
[Hiran Lakshika] <[hiranlakshika@gmail.com]>
2016-09-04 11:45:55 +0530

    Password Hashing with SHA1 added

--- app/src/main/java/com/example/hiran/onemarket/LoginActivity.java
+++ app/src/main/java/com/example/hiran/onemarket/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.example.hiran.onemarket;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,20 +35,61 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_log_in:
-                if (uname.getText().toString().equals("Hiran") && password.getText().toString().equals("123")) {
+                if (uname.getText().toString().equals("Hiran") && PasswordHash.encryptPassword(password.getText().toString()).equals("40bd001563085fc35165329ea1ff5c5ecbdbbeef")) {
                     Toast.makeText(this, "Login success", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(MainActivity.class.getName());
+                    uname.setText("");
+                    password.setText("");
                     startActivity(intent);
                 } else {
-                    Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Login Error", Toast.LENGTH_SHORT).show();
                 }
                 break;
             case R.id.sign_up:
-                Intent intent= new Intent(SignUpActivity.class.getName());
+                Intent intent = new Intent(SignUpActivity.class.getName());
                 startActivity(intent);
                 Toast.makeText(this, "Sign up", Toast.LENGTH_SHORT).show();
                 break;
         }
 
     }
+
+    //Changing the back key event
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+            // set title
+            alertDialogBuilder.setTitle("Alert !");
+
+            // set dialog message
+            alertDialogBuilder
+                    .setMessage("Do you want to exit?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // if this button is clicked, close
+                            // current activity
+                            LoginActivity.this.finish();
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // if this button is clicked, just close
+                            // the dialog box and do nothing
+                            dialog.cancel();
+                        }
+                    });
+
+            // create alert dialog
+            AlertDialog alertDialog = alertDialogBuilder.create();
+
+            // show it
+            alertDialog.show();
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
 }
