3c3afb9b2b52fb360caf4dcfb9c1b9c73c724882
[Hiran Lakshika] <[hiranlakshika@gmail.com]>
2016-09-11 11:22:17 +0530

    Item detail activity modified

--- app/src/main/java/com/example/hiran/onemarket/Activities/ItemDetailActivity.java
+++ app/src/main/java/com/example/hiran/onemarket/Activities/ItemDetailActivity.java
@@ -2,14 +2,28 @@ package com.example.hiran.onemarket.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.hiran.onemarket.R;
+import com.example.hiran.onemarket.Util.DBHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ItemDetailActivity extends AppCompatActivity {
+
+    private List<String> itemTitles = new ArrayList<>();
+    private TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_details);
+        title=(TextView)findViewById(R.id.item_name);
+        itemTitles= DBHelper.getInstance(this).getItems();
+
+        Bundle bundle = getIntent().getExtras();
+        int value = bundle.getInt("Position");
+        title.setText(itemTitles.get(value));
     }
 }
--- app/src/main/java/com/example/hiran/onemarket/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/hiran/onemarket/Fragments/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.hiran.onemarket.Fragments;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import com.example.hiran.onemarket.Activities.ItemDetailActivity;
 import com.example.hiran.onemarket.Adapters.HomeAdapter;
 import com.example.hiran.onemarket.R;
 import com.example.hiran.onemarket.Util.DBHelper;
@@ -23,7 +25,7 @@ public class HomeFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_layout, container, false);
         homeAdapter = new HomeAdapter(view.getContext(), DBHelper.getInstance(getActivity()).getPrices(), DBHelper.getInstance(getActivity()).getItems());
         homeGridView = (GridView) view.findViewById(R.id.home_grid_view);
@@ -32,8 +34,14 @@ public class HomeFragment extends Fragment {
         homeGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Bundle bundle = new Bundle();
+
                 Toast.makeText(getActivity(),
                         "Item Clicked: " + position, Toast.LENGTH_SHORT).show();
+                bundle.putInt("Position", position);
+                Intent intent = new Intent(getActivity(),ItemDetailActivity.class);
+                intent.putExtras(bundle);
+                getActivity().startActivity(intent);
             }
         });
         return view;
